1015960401db99220aa8df28645fe0836b0bb5d2
Gabriel Helko Meyer <ghmeyer0@gmail.com>
2020-02-28 23:40:09 -0300

    Initial commit

---
+++ CyberPet.Api/Models/Pet.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace CyberPet.Api.Models
+{
+    public class Pet
+    {
+        public Guid Id { get; set; }
+        public string PetName { get; set; }
+        [BsonDateTimeOptions]
+        public DateTime UpdatedOn { get; set; } = DateTime.Now;
+
+
+    }
+}
