0e135a05a35c829e70c50f866bdc373247e62c80
Gabriel Helko Meyer <ghmeyer0@gmail.com>
2020-02-29 16:05:25 -0300

    Adicionado Duplicação

--- CyberPet.Tests/Controllers/UsersControllerTest.cs
+++ CyberPet.Tests/Controllers/UsersControllerTest.cs
@@ -8,38 +8,83 @@ namespace CyberPet.Api.Controllers
 {
     public class UsersControllerTest
     {
-        //protected UsersController ControllerUnderTest { get; }
-        //protected Mock<IUserService> UserServiceMock { get;  }
+        protected UsersController ControllerUnderTest { get; }
+        protected Mock<IUserService> UserServiceMock { get;  }
 
-        //public UsersControllerTest()
-        //{
-        //    UserServiceMock = new Mock<IUserService>();
-        //    ControllerUnderTest = new UsersController(UserServiceMock.Object);
-        //}
+        public UsersControllerTest()
+        {
+            UserServiceMock = new Mock<IUserService>();
+            ControllerUnderTest = new UsersController(UserServiceMock.Object);
+        }
 
 
-        //public class ReadAllAsync : UsersControllerTest
-        //{
-        //    [Fact]
-        //    public async void Should_return_OkObjectResult_with_users()
-        //    {
-        //        //Arrange
-        //        var expectedUsers = new User[]
-        //        {
-        //            new User { Name = "Gabriel Meyer", Email = "ghmeyer0@gmail.com"},
-        //            new User { Name = "Renato Rezende", Email = "rrschiavo@gmail.com"}
-        //        };
-        //        UserServiceMock
-        //            .Setup(x => x.ReadAllAsync())
-        //            .ReturnsAsync(expectedUsers);
+        public class ReadAllAsync : UsersControllerTest
+        {
+            [Fact]
+            public async void Should_return_OkObjectResult_with_users()
+            {
+                //Arrange
+                var expectedUsers = new User[]
+                {
+                    new User { Name = "Gabriel Meyer", Email = "ghmeyer0@gmail.com"},
+                    new User { Name = "Renato Rezende", Email = "rrschiavo@gmail.com"}
+                };
+                var expectedUsers = new User[]
+                {
+                    new User { Name = "Gabriel Meyer", Email = "ghmeyer0@gmail.com"},
+                    new User { Name = "Renato Rezende", Email = "rrschiavo@gmail.com"}
+                };
+                var expectedUsers = new User[]
+                {
+                    new User { Name = "Gabriel Meyer", Email = "ghmeyer0@gmail.com"},
+                    new User { Name = "Renato Rezende", Email = "rrschiavo@gmail.com"}
+                };
+                var expectedUsers = new User[]
+                {
+                    new User { Name = "Gabriel Meyer", Email = "ghmeyer0@gmail.com"},
+                    new User { Name = "Renato Rezende", Email = "rrschiavo@gmail.com"}
+                };
+                var expectedUsers = new User[]
+                {
+                    new User { Name = "Gabriel Meyer", Email = "ghmeyer0@gmail.com"},
+                    new User { Name = "Renato Rezende", Email = "rrschiavo@gmail.com"}
+                };
+                var expectedUsers = new User[]
+                {
+                    new User { Name = "Gabriel Meyer", Email = "ghmeyer0@gmail.com"},
+                    new User { Name = "Renato Rezende", Email = "rrschiavo@gmail.com"}
+                };
+                var expectedUsers = new User[]
+                {
+                    new User { Name = "Gabriel Meyer", Email = "ghmeyer0@gmail.com"},
+                    new User { Name = "Renato Rezende", Email = "rrschiavo@gmail.com"}
+                };
+                var expectedUsers = new User[]
+                {
+                    new User { Name = "Gabriel Meyer", Email = "ghmeyer0@gmail.com"},
+                    new User { Name = "Renato Rezende", Email = "rrschiavo@gmail.com"}
+                };
+                var expectedUsers = new User[]
+                {
+                    new User { Name = "Gabriel Meyer", Email = "ghmeyer0@gmail.com"},
+                    new User { Name = "Renato Rezende", Email = "rrschiavo@gmail.com"}
+                };
+                var expectedUsers = new User[]
+                {
+                    new User { Name = "Gabriel Meyer", Email = "ghmeyer0@gmail.com"},
+                    new User { Name = "Renato Rezende", Email = "rrschiavo@gmail.com"}
+                };
+                UserServiceMock
+                    .Setup(x => x.ReadAllAsync())
+                    .ReturnsAsync(expectedUsers);
 
-        //        //Act
-        //        var result = await ControllerUnderTest.ReadAllAsync();
+                //Act
+                var result = await ControllerUnderTest.ReadAllAsync();
 
-        //        //Assert
-        //        var okResult = Assert.IsType<OkObjectResult>(result);
-        //        Assert.Same(expectedUsers, okResult.Value);
-        //    }
-        //}
+                //Assert
+                var okResult = Assert.IsType<OkObjectResult>(result);
+                Assert.Same(expectedUsers, okResult.Value);
+            }
+        }
     }
 }
