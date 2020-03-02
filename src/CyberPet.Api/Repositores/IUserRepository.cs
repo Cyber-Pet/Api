7c5aeb1cf285064e17bcc4deb1c6542d475c1682
Gabriel Meyer <ghmeyer0@gmail.com>
2020-03-02 19:27:11 -0300

    Implementado interface do repository de Users

---
+++ src/CyberPet.Api/Repositores/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using CyberPet.Api.Models;
+using MongoDB.Bson;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CyberPet.Api.Repositores
+{
+    interface IUserRepository
+    {
+        Task<IEnumerable<User>> ReadAllAsync();
+        Task<User> ReadOneAsync(ObjectId id);
+        Task<User> CreateAsync(User user);
+        Task<User> UpdateAsync(User user);
+        Task<User> DeleteAsync(ObjectId id);
+    }
+}
