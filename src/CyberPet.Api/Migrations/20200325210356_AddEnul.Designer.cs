2a9a42bf1216b724832e2f1ca1265bdf623cf245
Gabriel Meyer <ghmeyer0@gmail.com>
2020-03-25 18:04:27 -0300

    Adicionado Enumerador

--- src/CyberPet.Api/Migrations/20200325210356_AddEnul.Designer.cs
+++ src/CyberPet.Api/Migrations/20200325210356_AddEnul.Designer.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 using CyberPet.Api.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace CyberPet.Api.Migrations
 {
     [DbContext(typeof(CyberPetContext))]
-    partial class CyberPetContextModelSnapshot : ModelSnapshot
+    [Migration("20200325210356_AddEnul")]
+    partial class AddEnul
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/CyberPet.Api/Models/CyberPetContext.cs
+++ src/CyberPet.Api/Models/CyberPetContext.cs
@@ -36,6 +36,8 @@ namespace CyberPet.Api.Models
                 .HasForeignKey(s => s.PetId)
                 .OnDelete(DeleteBehavior.Cascade);
 
+            modelBuilder.HasPostgresEnum<DayOfWeek>();
+
 
             modelBuilder.Entity<User>().HasData(
                 new User { Id = Guid.Parse("bfbd39c6-76cb-4f49-8351-09ac4b64cb9c"), Email = "ghmeyer0@gmail.com", Name = "Gabriel Helko Meyer", Password = "4edc2113d0937fcc5f79c2f3af0a6aa30fa8fb545bfed7d06693d2c909399600", CreateAt = DateTime.Now, UpdateAt = DateTime.Now },
