3bbc8f65affcb5680ef53dd6e865d480a47b6e70
Gabriel Meyer <ghmeyer0@gmail.com>
2020-05-18 22:07:26 -0300

    Add field null

---
+++ src/CyberPet.Api/Migrations/20200519010704_Null.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CyberPet.Api.Migrations
+{
+    public partial class Null : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PetId",
+                table: "Bowl",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PetId",
+                table: "Bowl",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+        }
+    }
+}
